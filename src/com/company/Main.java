0c918dda0970e016b7dfa8231ce0e9c0530cf7df
tomm8231 <tomm8231@stud.kea.dk>
2022-02-28 15:53:28 +0100

    array added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,31 @@ package com.company;
 
 public class Main {
 
+    char[] letters = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M' , 'N', 'O', 'P', 'Q', 'R', 'S',
+        'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å'};
+
+   /* public char ceasarCryptLetterToNumber(char text, int shift) {
+
+    }
+
+    /* public int ceasarCryptNumberToLetter(char text, int shift) {
+
+    }
+
+    */
+
     public static void main(String[] args) {
+        Main obj = new Main();
+
+        //char testLetter = obj.ceasarCryptLetterToNumber('A',3);
+       // System.out.println(testLetter);
+
+        //int testNumber = obj.ceasarCryptNumberToLetter('A',3);
+        //System.out.println(testNumber);
+
+
+
 
     }
 }
